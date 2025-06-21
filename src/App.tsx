6b8fcc8f6017264bf374fac25594db8f11ecfa0f
apt <ap.138@outlook.com>
2025-06-21 06:26:06 +0000

    setup pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import Dashboard from "./pages/Dashboard";
 import GlobalStyle from "./GlobalStyles";
-import Button from "./ui/Button";
-import Heading from "./ui/Heading";
-import Input from "./ui/Input";
-import FlexRow from "./ui/FlexRow";
+import PageNotFound from "./pages/PageNotFound";
+import User from "./pages/User";
+import Setting from "./pages/Setting";
+import Booking from "./pages/Booking";
+import Cabin from "./pages/Cabin";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
     <>
       <GlobalStyle />
-      <FlexRow>
-        <Heading as="h1">Hello world</Heading>
-        <Button>Check-in</Button>
-      </FlexRow>
-      <Input type="number" placeholder="Number" />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="bookings" element={<Booking />} />
+          <Route path="cabins" element={<Cabin />} />
+          <Route path="account" element={<Account />} />
+          <Route path="users" element={<User />} />
+          <Route path="login" element={<Login />} />
+          <Route path="settings" element={<Setting />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
