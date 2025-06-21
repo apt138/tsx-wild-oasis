89cf8d14007c539cc5a707c579b3e9a69f8a5fb5
apt <ap.138@outlook.com>
2025-06-21 07:52:13 +0000

    added main navigation

---
+++ src/ui/MainNav.tsx
@@ -0,0 +1,91 @@
+import {
+  HiOutlineCalendar,
+  HiOutlineCog6Tooth,
+  HiOutlineHome,
+  HiOutlineHomeModern,
+  HiOutlineUsers,
+} from "react-icons/hi2";
+import { NavLink } from "react-router";
+import styled from "styled-components";
+
+const NavList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+`;
+
+const StyledNavLink = styled(NavLink)`
+  &:link,
+  &:visited {
+    padding: 1.2rem 2.4rem;
+    color: var(--color-grey-600);
+    font-size: 1.6rem;
+    font-weight: 500;
+    transition: all 0.3s;
+
+    display: flex;
+    align-items: center;
+    gap: 1.2rem;
+  }
+
+  & svg {
+    width: 2.4rem;
+    height: 2.4rem;
+    color: var(--color-grey-400);
+    transition: all 0.3s;
+  }
+
+  &:hover,
+  &.active:link,
+  &.active:visited {
+    color: var(--color-grey-800);
+    background-color: var(--color-grey-50);
+    border-radius: var(--border-radius-sm);
+  }
+
+  &:hover svg,
+  &:active svg,
+  &.active:link svg,
+  &.active:visited svg {
+    color: var(--color-brand-600);
+  }
+`;
+
+export default function MainNav() {
+  return (
+    <nav>
+      <NavList>
+        <li>
+          <StyledNavLink to="/dashboard">
+            <HiOutlineHome />
+            <span>Home</span>
+          </StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/bookings">
+            <HiOutlineCalendar />
+            <span>Bookings</span>
+          </StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/cabins">
+            <HiOutlineHomeModern />
+            <span>Cabins</span>
+          </StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/users">
+            <HiOutlineUsers />
+            <span>Users</span>
+          </StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/settings">
+            <HiOutlineCog6Tooth />
+            <span>Settings</span>
+          </StyledNavLink>
+        </li>
+      </NavList>
+    </nav>
+  );
+}
--- src/ui/SideBar.tsx
+++ src/ui/SideBar.tsx
@@ -1,14 +1,25 @@
 import styled from "styled-components";
+import Logo from "./Logo";
+import MainNav from "./MainNav";
 
 const StyledAside = styled.aside`
   background-color: var(--color-grey-0);
   padding: 3.2rem 2.4rem;
   border-right: 1px solid var(--color-grey-100);
 
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+
   grid-row: 1/-1;
   grid-column: 1;
 `;
 
 export default function SideBar() {
-  return <StyledAside>Side bar</StyledAside>;
+  return (
+    <StyledAside>
+      <Logo />
+      <MainNav />
+    </StyledAside>
+  );
 }
